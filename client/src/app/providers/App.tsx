19d9c05cd141ac47e0a1537569265af3fbf851ce
bonny <qhdms50@gmail.com>
2025-03-24 21:10:04 +0900

    init

---
+++ client/src/app/providers/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <article className="font-mimiWorld font-normal md:bg-bgColor bg-black">
+        
+      </article>
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ client/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from "path"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {find: "@", replacement: path.join(__dirname, "src")},
+      {find: "@img", replacement: path.join(__dirname, "src/assets/img")},
+    ]
+  },
+})

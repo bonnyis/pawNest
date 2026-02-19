510fd1b3d2978fe29f678e405a7405576e31a643
bonnyis <qhdms50@gmail.com>
2026-02-19 13:50:50 +0900

    불필요한 파일 삭제 및 font 재설정

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,22 +3,22 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./src/**/*.css" // ✅ CSS 파일 포함!
+    "./src/**/*.css", // ✅ CSS 파일 포함!
   ],
   theme: {
     extend: {
       fontFamily: {
-        mimiWorld: ["'1973MimiWorld'", "sans-serif"],
-      }
+        godick: ["Pretendard", "Noto Sans KR"],
+        display: ["'1973MimiWorld'", "Pretendard"],
+      },
+      colors: {
+        bgColor: "#FAF3E0",
+        oliveGr: "#A6A57A",
+        pinkColor: "#F7D4B4",
+        orgColor: "#F4A261",
+        grayColor: "gray",
+      },
     },
-    colors: {
-      bgColor: '#FAF3E0',
-      oliveGr: '#A6A57A',
-      pinkColor: '#F7D4B4',
-      orgColor:'#F4A261',
-      grayColor: "gray"
-    }
   },
   plugins: [],
-}
-
+};

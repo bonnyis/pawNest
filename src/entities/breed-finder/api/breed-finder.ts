9c631a1e58be53d35fc6a2640471784bc2fe0512
bonnyis <qhdms50@gmail.com>
2026-04-25 12:11:26 +0900

    [fix] 카드이미지 css 수정

--- src/entities/breed-finder/api/breed-finder.ts
+++ src/entities/breed-finder/api/breed-finder.ts
@@ -4,11 +4,7 @@ export const GET_BREED_FINDER = async (file: File) => {
   const formData = new FormData();
   try {
     formData.append("file", file);
-    const { data } = await api.post("/api/ai/detect-breed", formData, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const { data } = await api.post("/api/ai/detect-breed", formData);
     return data;
   } catch (error: any) {
     throw error.response?.data ?? error;

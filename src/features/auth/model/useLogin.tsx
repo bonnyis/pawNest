e5c2a68419dc4b6256087a81c1735ff4c58a3fa5
bonnyis <qhdms50@gmail.com>
2026-05-07 20:31:33 +0900

    [feat] 로그인 후 30분 만료 체크

--- src/features/auth/model/useLogin.tsx
+++ src/features/auth/model/useLogin.tsx
@@ -1,12 +1,46 @@
 import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useAuthStore } from "@/app/store/authStore";
+import { useAppStore } from "@/app/store/appStore";
 
 export const useLogin = () => {
   const [searchParams, updateSearchParams] = useSearchParams();
+
   const token = searchParams.get("token");
   const userId = searchParams.get("userId");
   const { updateToken, updateUserId, updateIsLogin } = useAuthStore();
+  const { updateIsAlertOpen } = useAppStore();
+  useEffect(() => {
+    const checkSession = () => {
+      const loginTime = parseInt(
+        sessionStorage.getItem("loginTime") || "0",
+        10,
+      );
+      if (!loginTime) {
+        return;
+      }
+      const now = new Date().getTime();
+
+      const elapsed = now - loginTime;
+      const thirtyMinutes = 30 * 60 * 1000;
+      if (elapsed > thirtyMinutes) {
+        sessionStorage.removeItem("loginTime");
+        updateIsLogin(false);
+        updateToken(null);
+        updateUserId(null);
+
+        updateIsAlertOpen({
+          flag: true,
+          message: "로그인 세션이 만료되었습니다. <br /> 다시 로그인해주세요.",
+        });
+      }
+    };
+    // 페이지가 로드될 때 세션 체크
+    checkSession();
+    const interval = setInterval(checkSession, 5 * 60 * 1000); // 5분마다 세션 체크
+    return () => clearInterval(interval);
+  }, []);
+
   useEffect(() => {
     if (!token || !userId) return;
     updateToken(token);
@@ -14,6 +48,8 @@ export const useLogin = () => {
     updateIsLogin(true);
 
     updateSearchParams({});
+
+    sessionStorage.setItem("loginTime", new Date().getTime().toString());
   }, [token, userId]);
 };
 

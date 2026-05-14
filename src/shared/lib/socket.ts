acab2b3aab9f79b920da13adaa663c832f32dec3
bonnyis <qhdms50@gmail.com>
2026-05-14 23:07:41 +0900

    [Fix] removeConsole 빌드 시 오류 수정

--- src/shared/lib/socket.ts
+++ src/shared/lib/socket.ts
@@ -1,5 +1,4 @@
 import { Client } from "@stomp/stompjs";
-// import SockJS from "sockjs-client";
 
 export const createSocketClient = (token: string) => {
   // 모바일 접속 시 ngrok의 웹소켓 지원이 불안정하여 SockJS 대신 기본 WebSocket 사용
@@ -12,14 +11,4 @@ export const createSocketClient = (token: string) => {
     reconnectDelay: 5000,
     debug: (str) => console.log("[STOMP]", str),
   });
-  // return new Client({
-  //   webSocketFactory: () =>
-  //     new SockJS("https://unbribably-unhilly-danyell.ngrok-free.dev/ws"),
-  //   connectHeaders: {
-  //     Authorization: `Bearer ${token}`,
-  //     "ngrok-skip-browser-warning": "69420",
-  //   },
-  //   reconnectDelay: 5000,
-  //   debug: (str) => console.log("[STOMP]", str),
-  // });
 };
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import removeConsole from "vite-plugin-remove-console";
-// https://vite.dev/config/
+
 export default defineConfig({
   define: { global: "window" },
-  plugins: [react(), removeConsole()],
+  plugins: [
+    react(),
+    removeConsole({
+      // 에러가 발생하는 파일이나 소켓 관련 파일 경로를 제외
+      external: ["src/shared/lib/socket.ts", "**/node_modules/**"],
+      includes: ["log"],
+    }),
+  ],
   resolve: {
     alias: [
       { find: "@", replacement: path.join(__dirname, "src") },

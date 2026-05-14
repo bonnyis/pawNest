192d12c17735288030e73f97094aef9e53821a1b
bonnyis <qhdms50@gmail.com>
2026-05-14 23:31:48 +0900

    [fix] 길찾기, 지도공유 url 규격 수정

--- src/features/shelter/model/useShareService.ts
+++ src/features/shelter/model/useShareService.ts
@@ -3,12 +3,12 @@ import type { ShelterItem } from "@/entities/shelter/model/shelter.types";
 export const useShareService = () => {
   // 길찾기 바로가기
   const handleAddress = (list: ShelterItem) => {
-    const url = `https://map.kakao.com/link/to/${list.ENTRPS_NM},${list.REFINE_WGS84_LOGT},${list.REFINE_WGS84_LAT}`;
-
+    const url = `https://map.kakao.com/link/to/${list.ENTRPS_NM},${list.REFINE_WGS84_LAT},${list.REFINE_WGS84_LOGT}`;
     window.open(url);
   };
+  // 지도 바로가기
   const returnAddress = (list: ShelterItem) => {
-    const url = `https://map.kakao.com/link/to/${list.ENTRPS_NM},${list.REFINE_WGS84_LOGT},${list.REFINE_WGS84_LAT}`;
+    const url = `https://map.kakao.com/link/map/${list.ENTRPS_NM},${list.REFINE_WGS84_LAT},${list.REFINE_WGS84_LOGT}`;
     return url;
   };
   return { handleAddress, returnAddress };
